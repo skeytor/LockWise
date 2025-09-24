53e20291b93c781afdf4771309fe1e59ef964e2e
Rober Guerrero <roberguerreromendoza@gmail.com>
2025-09-24 17:35:43 -0400

    feat: add login and sing up pages

--- LockWise/Data/ApplicationDbContext.cs
+++ LockWise/Data/ApplicationDbContext.cs
@@ -1,9 +1,7 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace LockWise.Data
-{
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
-    {
-    }
-}
+namespace LockWise.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+    : IdentityDbContext<ApplicationUser>(options);
